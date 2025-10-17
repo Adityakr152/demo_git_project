284f995b74bc7e567d557eee4d00024320206165
Adityakr152 <edugaonaditya@gmail.com>
2025-10-17 15:51:54 +0530

    my new projects for add homeScreen new branch add

---
+++ lib/sinup_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SinupPage extends StatefulWidget {
+  const SinupPage({super.key});
+
+  @override
+  State<SinupPage> createState() => _SinupPageState();
+}
+
+class _SinupPageState extends State<SinupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
